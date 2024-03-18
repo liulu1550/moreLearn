28cfaf9237234db2502cc62d3cc65b17f7dc1e13
liulu1550 <wouldmissyou@163.com>
2024-03-18 22:00:53 +0800

    login

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,7 @@
 function Common(){
     var self = this
     self.UserListHeaderItem = $(".right-wrapper .user-list-wrapper .wrapper-header > div")
+    self.registerBtn = $(".body-container .login-container .input-group .register-btn")
 }
 
 Common.prototype.listenUserListBtn = function (){
@@ -16,9 +17,37 @@ Common.prototype.listenUserListBtn = function (){
 }
 
 
+Common.prototype.listenRegisterBtnEvent = function (){
+    var self = this
+    self.registerBtn.on("click", function (){
+        var usernameInput = $(".body-container .input-group input[name='username']")
+        var passwordInput = $(".body-container .input-group input[name='password']")
+        var passwordInput2 = $(".body-container .input-group input[name='password2']")
+        console.log(usernameInput.val())
+        $.ajax({
+            "url":"/register/",
+            "method":"post",
+            "data":{
+                "username":usernameInput.val(),
+                "password":passwordInput.val(),
+                "password2":passwordInput2.val(),
+            },
+            "success":function (res){
+                if(res.code==200){
+                    alert(res.message)
+                }else {
+                    alert(res.message)
+                }
+            }
+        })
+    })
+}
+
+
 Common.prototype.run = function (){
     var self = this
     self.listenUserListBtn()
+    self.listenRegisterBtnEvent()
 }
 
 
